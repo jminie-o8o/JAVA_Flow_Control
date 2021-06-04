860e8550bb6270a542b647d2697fd18b87962867
jminie <jminie@naver.com>
2021-06-04 18:11:53 +0900

    JAVA_control_1

---
+++ BolleanApp.java
@@ -0,0 +1,21 @@
+
+public class BolleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true); //bollean 타입
+		System.out.println(false);
+		
+		String foo = "Hello World";
+//		String false = "Hello World"; //false는 변수의 이름으로 사용불가 이런 단어를 reserved word라고 한다.
+		
+		System.out.println(foo.contains("World"));
+		System.out.println(foo.contains("jminie"));
+
+
+	}
+
+}
