9308348bf15142b13cd22c785fd26f1100066f1b
jminie <jminie@naver.com>
2021-06-04 19:35:14 +0900

    JAVA_control_2

---
+++ IFApp.java
@@ -0,0 +1,18 @@
+
+public class IFApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("a");
+		if(false) {
+			System.out.println(1);
+		} else if(false) {
+			System.out.println(2);
+		}else {
+			System.out.println(3);
+		}
+		System.out.println("b");
+
+	}
+
+}
